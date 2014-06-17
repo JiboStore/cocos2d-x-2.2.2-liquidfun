6c882d7b81c65aa5fdd3a507f8def81383288cc3
KIM <hakim@tmgamer.co>
2014-06-17 16:43:48 +0800

    initial commit with unzip from: https://drive.google.com/#folders/0B8Onqgq9w6f5ZmQ4R2l1cVU2VGs

---
+++ projects/liquidfuntest/proj.wp8/liquidfuntest.cpp
@@ -0,0 +1,149 @@
+﻿#include <wrl/client.h>
+#include <d3d11_1.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <ppltasks.h>
+#include "liquidfuntest.h"
+#include "CCApplication.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Phone::UI::Input;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+USING_NS_CC;
+
+liquidfuntest::liquidfuntest()
+{
+}
+
+void liquidfuntest::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &liquidfuntest::OnActivated);
+
+	CoreApplication::Suspending +=
+		ref new EventHandler<SuspendingEventArgs^>(this, &liquidfuntest::OnSuspending);
+
+	CoreApplication::Resuming +=
+		ref new EventHandler<Platform::Object^>(this, &liquidfuntest::OnResuming);
+}
+
+void liquidfuntest::SetWindow(CoreWindow^ window)
+{
+    // Specify the orientation of your application here
+    // The choices are DisplayOrientations::Portrait or DisplayOrientations::Landscape or DisplayOrientations::LandscapeFlipped
+	DisplayProperties::AutoRotationPreferences = DisplayOrientations::Landscape;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &liquidfuntest::OnVisibilityChanged);
+
+	window->Closed += 
+		ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &liquidfuntest::OnWindowClosed);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &liquidfuntest::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &liquidfuntest::OnPointerMoved);
+
+	window->PointerReleased +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &liquidfuntest::OnPointerReleased);
+
+    CCEGLView* eglView = new CCEGLView();
+	eglView->Create(window);
+    eglView->setViewName("liquidfuntest");
+}
+
+void liquidfuntest::Load(Platform::String^ entryPoint)
+{
+}
+
+void liquidfuntest::Run()
+{
+    CCApplication::sharedApplication()->run();
+}
+
+void liquidfuntest::Uninitialize()
+{
+}
+
+void liquidfuntest::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnVisibilityChanged(sender, args);
+}
+
+void liquidfuntest::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnWindowClosed(sender, args);
+}
+
+void liquidfuntest::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerPressed(sender, args);
+}
+
+void liquidfuntest::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerMoved(sender, args);
+}
+
+void liquidfuntest::OnPointerReleased(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerReleased(sender, args);
+}
+
+void liquidfuntest::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	HardwareButtons::BackPressed += ref new EventHandler<BackPressedEventArgs^>(this, &liquidfuntest::OnBackButtonPressed);   
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void liquidfuntest::OnBackButtonPressed(Object^ sender, BackPressedEventArgs^ args)
+{
+    // Leave args->Handled set to false and the app will quit when user presses the back button on the phone
+}
+
+void liquidfuntest::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+	//m_renderer->ReleaseResourcesForSuspending();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void liquidfuntest::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+	// m_renderer->CreateWindowSizeDependentResources();
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new liquidfuntest();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
---
+++ samples/Cpp/TestCpp/Classes/Box2DTestBed/GLES-Render.h
@@ -0,0 +1,97 @@
+/*
+* Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
+* Copyright (c) 2014 Google, Inc
+*
+* iPhone port by Simon Oliver - http://www.simonoliver.com - http://www.handcircus.com
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "Box2D/Box2D.h"
+#include "cocos2d.h"
+
+// TODO: Need LiquidFun to expose a preprocessor symbol that is defined when
+// using the library vs. Box2D.
+#if defined(B2_ASSERT_ENABLED)
+#define USING_LIQUIDFUN 1
+#else
+#define USING_LIQUIDFUN 0
+#endif // defined(B2_ASSERT_ENABLED)
+
+struct b2AABB;
+
+// This class implements debug drawing callbacks that are invoked
+// inside b2World::Step.
+class GLESDebugDraw : public b2Draw
+{
+    float32 mRatio;
+    cocos2d::CCGLProgram* mUniformColorShaderProgram;
+    GLint        mUniformColorLocation;
+
+#ifdef USING_LIQUIDFUN
+    cocos2d::CCGLProgram* mNonUniformColorShaderProgram;
+	bool mSolidParticles;
+	bool mRenderParticles;
+#endif // USING_LIQUIDFUN
+
+    void initShader( void );
+public:
+    GLESDebugDraw();
+
+    GLESDebugDraw( float32 ratio );
+
+    virtual void DrawPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
+
+    virtual void DrawSolidPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
+
+    virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
+
+    virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
+
+#if USING_LIQUIDFUN
+    virtual void DrawParticles(const b2Vec2 *centers, float32 radius,
+                               const b2ParticleColor *colors, int32 count);
+#endif // USING_LIQUIDFUN
+
+    virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
+
+    virtual void DrawTransform(const b2Transform& xf);
+
+    virtual void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color);
+
+    virtual void DrawString(int x, int y, const char* string, ...); 
+
+    virtual void DrawString(const b2Vec2 &p, const char* string, ...) {
+        DrawString(p.x, p.y, string);
+    }
+
+    virtual void DrawAABB(b2AABB* aabb, const b2Color& color);
+
+#if USING_LIQUIDFUN
+    void SetSolidParticlesEnable(const bool enable) {
+      mSolidParticles = enable;
+	}
+
+	void SetRenderParticlesEnable(const bool enable) {
+	  mRenderParticles = enable;
+	}
+#endif // USING_LIQUIDFUN
+};
+
+
+#endif
